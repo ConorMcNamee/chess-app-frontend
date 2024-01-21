317387c247301fbe1ed2ff320a7d52a49b19b759
cmcnamee25 <cmcnamee4@gmail.com>
2024-01-21 15:35:17 +0000

    Completed chessboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import ChessBoard from './components/ChessBoard';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ChessBoard rows={8} columns={8} />
       </header>
     </div>
   );
--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,19 +1,129 @@
-import React, { useState } from 'react';
-import ChessSquare from "./ChessSquare"
+import { useEffect, useState } from "react";
+import "./ChessBoard.css";
 
-interface Chessboard {
-    id: string,
+interface BoardProps {
+    columns: number,
+    rows: number,
 }
 
-const ChessBoard: React.FC = () => {
+type PieceType = 'rook' | 'knight' | 'bishop' | 'queen' | 'king' | 'pawn';
+type PieceColor = 'black' | 'white';
 
-    const [chessboard, setChessboard] = useState();
+interface IChessPiece {
+    type: PieceType;
+    color: PieceColor;
+    code: string;
+    coordinates: string;
+}
+
+
+export default function ChessBoard({ columns, rows }: BoardProps) {
+    const files = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+    const pieceMap:IChessPiece[] = [
+        {"code": "r", "type": 'rook', color:"white", coordinates: "A1"},
+        {"code": "kn", "type": 'knight', color:"white", coordinates: "B1"},
+        {"code": "b", "type": 'bishop', color:"white", coordinates: "C1"},
+        {"code": "q", "type": 'queen', color:"white", coordinates: "D1"},
+        {"code": "k", "type": 'king', color:"white", coordinates: "E1"},
+        {"code": "b", "type": 'bishop', color:"white", coordinates: "F1"},
+        {"code": "kn", "type": 'knight', color:"white", coordinates: "G1"},
+        {"code": "r", "type": 'rook', color:"white", coordinates: "H1"},
+
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "A2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "B2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "C2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "D2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "E2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "F2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "G2"},
+        {"code": "p", "type": 'pawn', color:"white", coordinates: "H2"},
+
+        {"code": "R", "type": 'rook', color:"black", coordinates: "A8"},
+        {"code": "KN", "type": 'knight', color:"black", coordinates: "B8"},
+        {"code": "B", "type": 'bishop', color:"black", coordinates: "C8"},
+        {"code": "Q", "type": 'queen', color:"black", coordinates: "D8"},
+        {"code": "K", "type": 'king', color:"black", coordinates: "E8"},
+        {"code": "B", "type": 'bishop', color:"black", coordinates: "F8"},
+        {"code": "KN", "type": 'knight', color:"black", coordinates: "G8"},
+        {"code": "R", "type": 'rook', color:"black", coordinates: "H8"},
+
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "A7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "B7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "C7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "D7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "E7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "F7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "G7"},
+        {"code": "P", "type": 'pawn', color:"black", coordinates: "H7"},
+        
+    ]
+
+    const [board, setBoard] = useState<string[][]>(Array.from({ length: rows }, () =>
+        Array(columns).fill("")
+    ));
+
+  const setPosition = (coordinates:string) => {
+    let newBoard = [...board];
+    newBoard[0][1] = "rook";
+    setBoard(newBoard);
+  }
+
+  const getBoardIndicies = (coordinates:string): [number, number] => {
+    const col = files.indexOf(coordinates[0]);
+    const row = rows - parseInt(coordinates[1]);
+    return [row, col];
+  }
+
+  const setupBoard = (_board:string[][], _pieceMap:IChessPiece[]) => {
+    
+    let newBoard = [..._board];
+
+    _pieceMap.forEach(piece => {
+        const [row, col] = getBoardIndicies(piece.coordinates);
+        newBoard[row][col] = piece.code;
+    })
+    setBoard(newBoard);
+  }
+
+    const clearBoard = (board:string[][]) => {
+        for(let i = 0; i < board.length; i++) {
+            for(let j = 0; j < board[i].length; j++) {
+            }
+        }
+    }
+
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();      
+    }
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+
+        const boardX = event.currentTarget.className.split(' ')[1]      
+        const boardY = event.currentTarget.className.split(' ')[2] 
+        console.log("Setting at: ", boardX, boardY)
+    }
+
+    useEffect(() => {
+        setupBoard(board, pieceMap);
+    }, [setBoard]);
 
     return (
-        <div>
-            <h2>Chess Board</h2>
+        <div className="board">
+            {board.map((row, rowIndex) => (
+                <div key={rowIndex} className="row">
+                    {row.map((cell, colIndex) => (
+                        <div
+                            key={colIndex}
+                            className={`cell ${files[colIndex]}${rows - rowIndex}`}
+                            onDragOver={handleDragOver}
+                            onDrop={handleDrop}>
+                            {cell && <span className="piece">{cell}</span>}
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
-
-export default ChessBoard
